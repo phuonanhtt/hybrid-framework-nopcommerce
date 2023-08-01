ce35dc52a3f1c8cb065048e25b9800715e822ead
Phuong anh <phuonganhttcb@gmail.com>
2023-08-01 17:51:22 +0700

    bai 50

--- testcases/com/nopcommerce/user/Level_01_Register.java
+++ testcases/com/nopcommerce/user/Level_01_Register.java
@@ -6,27 +6,38 @@ import org.testng.annotations.AfterClass;
 
 public class Level_01_Register {
 
-  @BeforeClass
-  public void beforeClass() {
-	  
-  }
-  
-  @Test
-  public void Register_01_Empty_Data() {
-	  
-  }
-  
-  @Test
-  public void Register_02_Invalid_Email() {
-	  
-  }
-  @Test
-  public void Register_03_Invalid_Password() {
-	  
-  }
+	@BeforeClass
+	public void beforeClass() {
 
-  @AfterClass
-  public void afterClass() {
-  }
+	}
+
+	@Test
+	public void Register_01_Empty_Data() {
+
+	}
+
+	@Test
+	public void Register_02_Invalid_Email() {
+
+	}
+
+	@Test
+	public void Register_03_Invalid_Password() {
+
+	}
+
+	@Test
+	public void Register_04_Incorect_Confirm_Password() {
+
+	}
+
+	@Test
+	public void Register_05_Incorect_Confirm_Password() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
 
 }
